7ca33e7320a40647698677599826f67cbac69d01
gogobossStampolidis <82140482+gogobossStampolidis@users.noreply.github.com>
2021-04-21 16:44:12 +0300

    Update Messenger.bat

--- Messenger.bat
+++ Messenger.bat
@@ -1,10 +1,31 @@
 echo off
 cls
-
+echo.
+echo.
 echo This is for viewing messages. Open Send.bat to send a message
 echo ==============================================================
-pause
-echo Welcome To The Chat!
+echo.
+echo.
+echo.
+echo   000000      000000     00000       000000
+echo  00          00          00   0     00
+echo  00          00          00   00    00
+echo  00   __     00   __     00000000    000000
+echo  00     0    00     0    00   00          00
+echo  00     0    00     0    00   0           00
+echo   000000      000000     00000       000000
+echo.
+echo              STUDIOS
+echo.
+echo.
+echo     THIS PROGRAM IS CALLED : 
+echo.
+echo      CMSFL WHICH STANDS FOR 
+echo.
+echo   CMD-MESSAGING-SYSTEM-for-LAN
+echo.
+echo.
+PAUSE
 :Chat
 timeout 1 /NOBREAK>nul
 cls
