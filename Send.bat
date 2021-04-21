0a1193aa9db5de2e1f0ce6535a9150ae2857453f
gogobossStampolidis <82140482+gogobossStampolidis@users.noreply.github.com>
2021-04-21 09:14:42 +0300

    Update Send.bat

--- Send.bat
+++ Send.bat
@@ -3,16 +3,20 @@ cls
 
 echo What is your user name?
 set /P UserName=}_
-echo [SYSTEM]: [%UserName%] Has Joined The Chat Room!
+echo [SYSTEM]: [%UserName%] Has Joined The Chat Room! >> Chat.txt
 cls
-:Send
 echo Type $Help for commands!
 echo Logged In As [%UserName%]
+:Send
+
+
 echo Send A Message: 
 set /P SendMessage=}_
 if /I "%SendMessage%" EQU "$Help" goto :Help (
 	) ELSE (
 		if /I "%SendMessage%" EQU "$Leave" goto :Leave
+			) ELSE (
+		if /I "%SendMessage%" EQU "$Broadcast" goto :Broadcast
 			) ELSE (
 				echo [%UserName%]: %SendMessage% >> Chat.txt
 goto Send
@@ -23,8 +27,17 @@ echo ========================
 echo $Leave
 echo   Leave The Chat Room D=
 echo ========================
+echo $Broadcasts
+echo   Broadcast something!
+echo ========================
 echo That Is Everything!
 goto :Send
 
 :Leave
 echo [SYSTEM]: [%UserName%] Has Left The Chat Room! >> Chat.txt
+
+:Broadcast
+echo Enter text to broadcast!
+set /P BroadcastMessage=}_
+echo [BROADCAST SYSTEM]: [%UserName%] Says %BroadcastMessage% >> Chat.txt
+goto Send
